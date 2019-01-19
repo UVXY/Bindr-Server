5216ee8e7991092f1e3f1e36333609e8d52f583a
Erik Salvatier <m.erik.salvatier@gmail.com>
2019-01-19 11:08:39 -0800

    audio uploading

--- routes/comments/index.js
+++ routes/comments/index.js
@@ -1,7 +1,16 @@
+require('dotenv').config()
+
 const express = require('express');
 const router = express.Router();
 const multer  = require('multer');
+const cloudinary = require('cloudinary');
 const db = require('../../db/models');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -15,5 +24,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 router.post("/comment/audio", upload.single("audio-comment"), (req, res) =>{
+
   
 });
